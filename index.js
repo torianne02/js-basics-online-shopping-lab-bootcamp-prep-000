da35a5806e0cdffb8d334181d5554768dd3b561c
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-18 22:17:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i <cart.length; i++) {
     if (cart[i] === item) {
-      cart.splice()
+      cart.splice(i, 1);
     }
   }
 }
