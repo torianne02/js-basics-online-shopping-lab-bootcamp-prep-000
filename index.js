dcde17c8e7fbe2040588c817bbcaf5699f226e62
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-18 22:20:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,8 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i <cart.length; i++) {
-    if (cart[i] === item) {
+    var cartItem = cart[i];
+    if (cartItem['itemName'] === item) {
       cart.splice(i, 1);
       return cart;
     }
