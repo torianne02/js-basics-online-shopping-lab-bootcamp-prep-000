dbfe14b30866c3da4325f2c92bd7d78c1179d6e6
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-03-18 20:46:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function addToCart(item) {
    'itemPrice': Math.floor(Math.random() * 100) + 1
  }
  cart.push(obj);
- return "${item} has been added to your cart."
+ return item + " has been added to your cart."
 }
 
 function viewCart() {
